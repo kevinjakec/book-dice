f28d5e088e7006cb66aef2cc0e54c3bbe4b6df75
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-13 17:43:24 +0800

    Add book CRUD functions

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :publish_date, presence: true
+    validates :isbn_number, presence: true
 end
