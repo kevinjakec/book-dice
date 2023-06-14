966ac8328acda299ac8bfbdc0d0e57b779a7c1df
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-14 13:58:26 +0800

    add reviews mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'books#index'
 
-  resources :books
+  resources :books do
+    resources :reviews
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_060305) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_234055) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -25,24 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_060305) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "friends", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+  create_table "reviews", force: :cascade do |t|
+    t.string "reviewer"
+    t.text "body"
+    t.integer "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+  add_foreign_key "reviews", "books"
 end
