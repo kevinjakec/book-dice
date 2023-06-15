b16879cac0c536b764209f07695fcae59e80d59a
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-15 11:59:32 +0800

    edit user views and add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  
   devise_for :users
   resources :comments
   resources :reviews
-  root 'books#index'
   resources :books
   resources :genres
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
