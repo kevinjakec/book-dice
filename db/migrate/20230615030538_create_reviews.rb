dc01f406937a78ae9ba0679aa00e09770a732d35
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-27 12:49:03 +0800

    edit db schema foreign_keys to default

--- db/migrate/20230615030538_create_reviews.rb
+++ db/migrate/20230615030538_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.references :book, null: false, foreign_key: true
-      t.references :reviewer, null: false, foreign_key: { to_table: :users }
+      t.references :reviewer, null: false, foreign_key: true
       t.text :body
       t.references :comments, null: true, foreign_key: true
       t.timestamps
