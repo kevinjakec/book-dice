39a9645a1188e8eb614c0f815d1cfc73357843c8
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-27 12:12:52 +0800

    minimize build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-RAILS_ENV=production bundle exec rake db:drop db:create db:migrate db:seed
+bundle exec rake db:migrate db:seed
