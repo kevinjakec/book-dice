a035eaa1abcb87258ac38b527feec7da68ba932f
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-28 12:21:10 +0800

    update db schema

--- db/migrate/20230628004713_add_genre_id_to_books.rb
+++
@@ -1,6 +0,0 @@
-class AddGenreIdToBooks < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :books, :genre, null: false, foreign_key: { to_table: :genres, column: :genre_id }
-    add_reference :books, :user, null: false, foreign_key: { to_table: :users, column: :submitted_by }
-  end
-end
