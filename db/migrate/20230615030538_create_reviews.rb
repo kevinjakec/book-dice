4897ffd17e0abdd40db077c69540789b285d47f8
kevinjakec <kevin.casala@mrisoftware.com>
2023-06-28 13:17:40 +0800

    add , deferred: true option

--- db/migrate/20230615030538_create_reviews.rb
+++ db/migrate/20230615030538_create_reviews.rb
@@ -2,9 +2,9 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.references :book, null: false, foreign_key: true
-      t.references :reviewer, null: false, foreign_key: { to_table: :users }
+      t.references :reviewer, null: false, foreign_key: { to_table: :users, deferred: true }
       t.text :body
-      t.references :comments, null: true, foreign_key: true
+      t.references :comments, null: true, foreign_key: true, deferred: true
       t.timestamps
     end
   end
